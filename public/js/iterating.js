f4b1ff2304cc57d6320371a84f248e9f58727d5f
ICON <ICON@news-MacBook-Pro.local>
2017-06-19 14:13:03 -0500

    Adding drills from today.

--- public/js/iterating.js
+++ public/js/iterating.js
@@ -1,15 +1,52 @@
 (function(){
     "use strict";
+    //
+    // // // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+    // //
+    // var names = ['John', 'Paul', 'George', 'Ringo'];
+    // // // TODO: Create a log statement that will log the number of elements in the names array.
+    // console.log(names.length);
+    // //
+    // // // TODO: Create log statements that will print each of the names array elements individually.
+    // console.log(names[0]);
+    // console.log(names[1]);
+    // console.log(names[2]);
+    // console.log(names[3]);
+    //
+    // for (var i = 0; i < names.length; i++) {
+    //   console.log(names[i]);
+    // }
+    //
+    // names.forEach(function (element) {
+    //   console.log(element);
+    // });
 
-    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
-    var names = ['John', 'Paul', 'George', 'Ringo'];
+    var a = 1;
+    var temperatures = new Array ();
+    var array2 = new Array ();
+    var measure;
+    for (var i = 0; i < 5; i++){
+      temperatures[i] = prompt('number');
+      } console.log(temperatures);
+      measure = prompt('If you want to convert Fahrenheit to Celsius, type "Celsius". \n If you want to convert to Celsius to Fahrenheit, type "Fahrenheit".' );
+      console.log(measure);
 
-    // TODO: Create a log statement that will log the number of elements in the names array.
-    console.log(names.length);
+      if (measure == 'Fahrenheit'){
+        temperatures.forEach(function (numbers, index) {
+          numbers = parseInt(numbers -32 * 5/9);
+          // temperatures[index] = numbers;
 
-    // TODO: Create log statements that will print each of the names array elements individually.
-    console.log(names[0]);
-    console.log(names[1]);
-    console.log(names[2]);
-    console.log(names[3]);
+          array2.push(numbers);
+          // array2[] = value;
+
+        })
+      } else {
+        temperatures.forEach(function (numbers, index) {
+          numbers = parseInt(numbers * 9/5 + 32);
+          array2.push(numbers);
+        })
+      };
+      console.log(temperatures);
+      console.log(array2);
+
 })();
---
+++ public/js/mad_libs.js
@@ -0,0 +1,16 @@
+(function(){
+
+Pick a number 1-10.
+Enter a job name.
+Enter a new job.
+Enter the name of a city.
+Enter a person's name.
+
+
+
+
+
+
+      console.log(() +  'years after the end of Rush Hour 2, James Carter is no longer a' + () + ,' but a' + () +  'on the streets of' + () + '. Lee is now the bodyguard for his friend' () + '. Lee is still upset with Carter about an incident in' + () +  'when Carter accidentally shot Lee`s girlfriend,' + () +  'Isabella Molina, in the' () + '.')
+
+})();
