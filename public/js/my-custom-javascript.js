beec55c31dcf54a645a6561739c0ffa988a7c5dd
ICON <ICON@news-MacBook-Pro.local>
2017-06-27 14:50:09 -0500

    deleted old jQuery from exercise. Starting new exercise.

--- public/js/my-custom-javascript.js
+++ public/js/my-custom-javascript.js
@@ -2,21 +2,5 @@
 
 $(document).ready(function() {
 
-    // $('#theBody')
-    // $('#process')
-    // var contents = $('#process').html();
-    // alert(contents);
-    // var bodyContents = $('#theBody');
-    // alert(bodyContents);
-    // var increaseSuccess = $('#increaseSuccess').html();
-    // alert(increaseSuccess);
-
-    // $('.codeup').css('border','2px solid red')
-
-    $('li').css('font-size','20px');
-    $('h1, p, li').css('background-color', 'yellow');
-    $('body').css('background-image', "url(/img/smiley.jpg)").css('background-size','10%');
-    var alertH1 = $('h1').html();
-    alert(alertH1);
 
 });
