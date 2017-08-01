0401c0f8b22c3ac0e6962952db1e4d17911888dc
Hunter Warren <hunter.warren@gmail.com>
2017-08-01 10:29:49 -0500

    counter exercise works!

---
+++ public/counter.php
@@ -0,0 +1,52 @@
+<?php
+
+function pageController() {
+  $count = (isset($_GET["count"])) ? $_GET["count"] : 0;
+  $dataArray = [
+    "count" => $count
+  ];
+  return $dataArray;
+}
+
+extract(pageController());
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <style media="screen">
+  .bigdiv {
+    text-align:center;
+    margin-top: 100px;
+    margin-left: auto;
+  }
+  h1 {
+    color: black;
+    font-size: 200px;
+    font-family: "Helvetica";
+  }
+  a {
+    color: blue;
+    font-size: 60px;
+    font-family: "Helvetica";
+    padding: 20px;
+  }
+  </style>
+</head>
+<body>
+  <div class="bigdiv">
+    <h1><?= $count ?></h1>
+    <a href=
+    <?php
+    $count++;
+    echo "?count=".$count;?>
+    >_UP_</a>
+    <a href=
+    <?php
+    $count-=2;
+    echo "?count=".$count;?>
+    >DOWN</a>
+  </div>
+</body>
+</html>
