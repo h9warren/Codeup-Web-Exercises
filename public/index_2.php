12b8b1807fb92a883f726a0c4c7e0667ced908b3
Hunter Warren <hunter.warren@gmail.com>
2017-08-03 11:12:08 -0500

    completed first templating exercise.

---
+++ public/index_2.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'functions.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  </head>
+  <body>
+    <?php include 'nav.php'; ?>
+    <?php include 'body.php'; ?>
+    <?php include 'footer.php'; ?>
+  </body>
+</html>
