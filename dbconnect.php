0de2faf4608100fb218fb3363d01a55d79815f2b
Hunter Warren <hunter.warren@gmail.com>
2017-08-10 15:18:04 -0500

    starting new parks database

--- dbconnect.php
+++ dbconnect.php
@@ -2,10 +2,8 @@
 
 	require_once "env.php";
 
-	$dbc = new PDO("mysql:host=127.0.0.1;dbname=".dbName, username, password);
+	$dbc = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
 
 	$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	
 
 ?>
